7acc1566f9b0d24c62bc4a9e43ac1c4981f2e9cf
maramar0414 <maramar0414@gmail.com>
2023-12-30 16:47:57 +0000

    icon stoties added

---
+++ web/src/components/Icon/Icon.stories.tsx
@@ -0,0 +1,102 @@
+// Pass props to your component by passing an `args` object to your story
+//
+// ```tsx
+// export const Primary: Story = {
+//  args: {
+//    propName: propValue
+//  }
+// }
+// ```
+//
+// See https://storybook.js.org/docs/react/writing-stories/args.
+
+//importing content
+import type { Meta, StoryObj } from '@storybook/react'
+
+import Icon from './Icon'
+
+const meta: Meta<typeof Icon> = {
+  component: Icon,
+}
+
+export default meta
+
+type Story = StoryObj<typeof Icon>
+
+export const Calendar: Story = {
+  args: {
+    id:'calendar',
+  }
+}
+
+export const Check: Story = {
+  args: {
+    id:'check',
+  }
+}
+
+export const Chevron: Story = {
+  args: {
+    id:'chevron',
+  }
+}
+
+export const Close: Story = {
+  args: {
+    id:'close',
+  }
+}
+
+export const EyeClosed: Story = {
+  args: {
+    id:'eyeClosed',
+  }
+}
+
+export const EyeOpened: Story = {
+  args: {
+    id:'eyeOpened',
+  }
+}
+
+export const LogOut: Story = {
+  args: {
+    id:'logOut',
+  }
+}
+
+export const Minus: Story = {
+  args: {
+    id:'minus',
+  }
+}
+
+export const Plus: Story = {
+  args: {
+    id:'plus',
+  }
+}
+
+export const Question: Story = {
+  args: {
+    id:'question',
+  }
+}
+
+export const thumbsDown: Story = {
+  args: {
+    id:'thumbsDown',
+  }
+}
+
+export const ThumbsUp: Story = {
+  args: {
+    id:'thumbsUp',
+  }
+}
+
+export const Upload: Story = {
+  args: {
+    id:'upload',
+  }
+}
