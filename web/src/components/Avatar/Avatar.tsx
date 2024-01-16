27a643c57fba8cc3c4aeeed159df95597b4113c6
maramar0414 <maramar0414@gmail.com>
2024-01-16 21:14:00 +0000

    Santa hat added to Avatar

--- web/src/components/Avatar/Avatar.tsx
+++ web/src/components/Avatar/Avatar.tsx
@@ -2,7 +2,7 @@ import Icon from "../Icon/Icon"
 
 const Avatar = () => {
   return (
-    <div>
+    <div className="relative">
       <img 
       src="/images/copy.jpg" 
       alt="Marie font"
@@ -14,11 +14,18 @@ const Avatar = () => {
       <div className="center h-18 w-18 rounded-full border-avatar border-vistablue bg-spanishGreen text-2xl font-bold uppercase text-red-100">
         <Icon id="eyeClosed" size={32}/>
       </div>
+      <div className="relative inline-block">
+      <img 
+      src="/images/copy.jpg" 
+      alt="Marie font"
+      className="h-18 w-18 rounded-full border-avatar border-white bg-spanishGreen text-2xl font-bold uppercase text-white"
+      ></img>
       <img 
       src="/images/santa-hat.png" 
       alt="Santa-hat"
-      className="absolute -right-[15px] -top-[15px] h-10 w-10 rounded-full border-avatar border-white bg-spanishGreen text-2xl font-bold uppercase text-white"
+      className="absolute -right-[15px] -top-[15px] "
       ></img>
+      </div>
       </div>
   )
 }
