2b22fc80dc7c57f910cb80c7d9869b3f02bae3e9
maramar0414 <maramar0414@gmail.com>
2024-01-17 11:50:10 +0000

    Interface added

--- web/src/components/Indicator/Indicator.tsx
+++ web/src/components/Indicator/Indicator.tsx
@@ -1,23 +1,27 @@
-import Icon from "../Icon/Icon"
+import Icon from '../Icon/Icon'
 
-const Indicator = ({status}) => {
+interface IndicatorProps {
+  status: 'success' | 'error' | 'warning'
+}
+
+const Indicator = ({ status }: IndicatorProps) => {
   return (
     <>
     {/*success*/}
     {status === "success" && (<div className="rounded-full center h-6 w-6 border-2 border-white bg-spanishGreen text-white">
         <Icon id="check" size={16} />
       </div>)}
-      
+
       {/*error*/}
       {status === "error" && (<div className="rounded-full center h-6 w-6 border-2 border-white bg-orangeRed text-white">
         <Icon id="minus" size={16} />
       </div>)}
-      
+
         {/*warning*/}
         {status === "warning" && (<div className="rounded-full center h-6 w-6 border-2 border-white bg-supernova text-black">
         <Icon id="question" size={16} />
       </div>)}
-     
+
       </>
   )
 }
